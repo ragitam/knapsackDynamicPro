1bebd257c3e131d7672af997325798595b968857
ragitam <agitarachmad@students.telkomuniversity.ac.id>
2019-05-04 11:44:54 +0700

    update

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -2,14 +2,8 @@ import java.util.Scanner;
 
 public class Knapsack {
 
-    static int findMax(int a, int b){
-        if(a > b){
-            return a;
-        }else
-            return b;
-    }
-
     static void tabelKnapsack(int kapasitas, int[] berat, int[] nilai, int in){
+        //method untuk menghitung knapsack
         int i, w;
         int tas[][] = new int[in+1][kapasitas+1];
 
@@ -19,7 +13,7 @@ public class Knapsack {
         System.out.println();
         System.out.println("-----------------------------------------");
 
-        //Menyimpan data barang yang diambil dan tidak dalam bentuk tabel/matriks
+        //Menyimpan data nilai keuntungan yang diperoleh dari barang yang diambil dalam bentuk tabel/matriks
         for(i=0; i<=in; i++){
             for(w=0; w<=kapasitas; w++){
                 if(i == 0 || w == 0){
@@ -29,10 +23,13 @@ public class Knapsack {
                 }else if(berat[i-1] <= w){
                     //case 2 : kondisi kapasitas tas dapat memuat barang saat ini
                     //ketika berat barang saat ini (berat[i-1]) <= kapasitas tas (w)
-                    tas[i][w] = findMax((nilai[i-1]+tas[i-1][w - berat[i-1]]), tas[i-1][w]);
+                    //mencari nilai maksimum dari nilai barang saat ini ditambah dengan nilai keuntungan knapsack dan
+                    //nilai kondisi tas sebelum barang saat ini masuk
+                    tas[i][w] = Math.max((nilai[i-1]+tas[i-1][w - berat[i-1]]), tas[i-1][w]);
                 }else{
                     //case 3 : kondisi kapasitas tas tidak dapat memuat barang
                     //ketika berat barang saat ini (berat[i-1]) > kapasitas tas (w)
+                    //maka yang diambil adalah barang yang sebelumnya
                     tas[i][w] = tas[i-1][w];
                 }
                 System.out.print(tas[i][w] +"\t" +"|\t");
@@ -40,6 +37,7 @@ public class Knapsack {
             System.out.println();
         }
 
+        //menyimpan hasil knapsack kedalam variabel baru
         int hasilAkhir = tas[in][kapasitas];
         System.out.println("-----------------------------------------");
         System.out.println("Hasil Knapsack : " +hasilAkhir);
@@ -50,8 +48,10 @@ public class Knapsack {
         //Menulis kesimpulan barang yang diambil
         for(i=in; i>0 && hasilAkhir>0; i--){
             if(hasilAkhir == tas[i-1][w]){
+                //jika kondisi hasil akhir knapsack sesuai dengan isi tas saat ini
                 continue;
             }else{
+                //mengambil nilai barang yang diambil
                 System.out.println("Barang ke-" +i +" berat : " +berat[i-1] +" nilai : " +nilai[i-1]);
 
                 hasilAkhir = hasilAkhir-nilai[i-1];
@@ -80,7 +80,6 @@ public class Knapsack {
         int kapasitas = s.nextInt();
 
         tabelKnapsack(kapasitas, berat, nilai, in);
-
     }
 }
 
